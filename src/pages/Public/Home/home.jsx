616a272301f6654374a3a0213ada89f082f2c391
Philippe De Oliveira <philippe.deoliveira75@gmail.com>
2026-03-13 11:51:55 +0000

    text about us

--- src/pages/Public/Home/home.jsx
+++ src/pages/Public/Home/home.jsx
@@ -50,8 +50,8 @@ export default function Home() {
           <div className="home__about-text">
             <h2>À propos de nous</h2>
             <p>
-              Nous sommes producteurs locaux à Metz en Moselle, passionnés par des légumes frais, sains et de saison.
-              Notre objectif est de vous offrir le meilleur de la nature, directement à votre table.
+              Les Jardins du Val de Moselle font pousser des légumes depuis six générations, avec le même attachement au goût et à la fraîcheur. À Metz et La Maxe, nous produisons sur 15 hectares en agriculture biologique, en privilégiant des pratiques qui préservent les sols, la biodiversité et l’environnement, tout en offrant une large une production au fil des saisons, des légumes croquants aux tomates bien mûres, sans oublier les fraises fraîchement cueillies.
+Aujourd’hui pilotée par Sébastien Obriot, l’exploitation continue d’avancer avec l’envie de proposer aux Messins de bons fruits et légumes sains, locaux et frais, cultivés dans le respect de l’environnement, du champ à l’assiette !
             </p>
           </div>
           <div className="home__about-image">
